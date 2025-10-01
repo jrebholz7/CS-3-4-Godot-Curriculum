6490c77f7622b73ee36a46a1b1ea1654d8afe740
jrebholz7 <jrebholz7969@student.pps.net>
2025-10-01 14:06:47 -0700

    worked on stuff in slime, when you get back ask ansel to help you fix player position

--- scripts/enemies/enemy_base.gd
+++ scripts/enemies/enemy_base.gd
@@ -2,7 +2,9 @@ extends npc
 
 @onready var sprite: Sprite2D = $Sprite2D
 
-
+var can_damage = true
+var damage_amount:int = 0
+var damage_cooldown:float = 0
 @export var move_speed: float = 5
 
 
@@ -15,3 +17,29 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 	# MOVE TWOARD PLAYER
+func configure_pickup(_type: String) -> bool:
+	if _type == "Slime":
+		damage_amount == 1
+		damage_cooldown == 2
+		return true
+	else: return false
+func _on_detection_radius_body_entered(body: Node2D) -> void:
+
+	if body is Player:
+		is_hostile = true
+
+
+func _on_detection_radius_body_exited(body: Node2D) -> void:
+	if body is Player:
+		is_hostile = false
+
+
+func _on_hit_area_body_entered(body: Node2D) -> void:
+		if body is Player and can_damage:
+			body.change_health(damage_amount)
+		get_tree().create_timer(damage_cooldown).timeout
+
+
+
+#func _on_hit_area_body_exited(body: Node2D) -> void:
+#	pass # Replace with function body.
--- scripts/npc.gd
+++ scripts/npc.gd
@@ -25,13 +25,6 @@ func _physics_process(delta: float) -> void:
 	pass
 	
 
-func _on_detection_radius_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
-
-
-func _on_detection_radius_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
-
 
 
 func movement(_delta):
