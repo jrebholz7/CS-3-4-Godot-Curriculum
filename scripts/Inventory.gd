5575099bd1d3b8bd7a56359730f1cfef9007488a
jrebholz7 <jrebholz7969@student.pps.net>
2025-10-22 14:04:12 -0700

    worked on inventory, got it applied but didn't get it working, FIX INVENTORY

--- scripts/Inventory.gd
+++ scripts/Inventory.gd
@@ -2,10 +2,14 @@ extends Node2D
 var inventory: Array[Inventory_Items] = []
 var selected_item : int = 0
 
+func _on_area_2d_body_entered(body):
+	if body is Player:
+		print_inventory_item(selected_item)
 func _ready() -> void:
-	print_inventory_item(selected_item)
+	inventory.append("health potion")
+	#print_inventory_item(selected_item)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("inventory_up"):
 		selected_item -=1
 		if selected_item <0:
@@ -19,16 +23,16 @@ func _process(delta: float) -> void:
 
 
 func print_inventory_item(item_number : int):
-	print("The item is called " +inventory[item_number].name)
-	print("The item costs " +str(inventory[item_number].cost))
-	print("I have " +str(inventory[item_number].quantity) + " of this item")
-	print("This item " +inventory[item_number].description)
+	print("The item is called " + inventory[item_number].name)
+	print("The item costs " + str(inventory[item_number].cost))
+	print("I have " + str(inventory[item_number].quantity) + " of this item")
+	print("This item " + inventory[item_number].description)
 
-func add_item(inventory_item : Inventory_Items):
-	pass
-
-func delete_item():
-	pass
+#func add_item(inventory_item : Inventory_Items):
+	#pass
+#
+#func delete_item():
+	#pass
 
 # add to inventory
 # remove from inventory
