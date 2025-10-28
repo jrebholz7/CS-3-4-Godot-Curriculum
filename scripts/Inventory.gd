f7c9df2c03f7559da4d82785e3c2eb81acacfb4a
jrebholz7 <jrebholz7969@student.pps.net>
2025-10-28 13:55:03 -0700

    finished everything

--- scripts/Inventory.gd
+++ scripts/Inventory.gd
@@ -9,6 +9,12 @@ func _on_area_2d_body_entered(body):
 
 
 func _ready() -> void:
+	var sword = preload("res://Items/sword.tres")
+	var potion = preload("res://Items/Potion.tres")
+	var coin = preload("res://Items/coin.tres")
+	add_item(potion)
+	add_item(sword)
+	add_item(coin)
 	print_inventory_item(selected_item)
 
 func _process(_delta: float) -> void:
@@ -22,6 +28,15 @@ func _process(_delta: float) -> void:
 		if selected_item >inventory.size()-1:
 			selected_item = inventory.size()-1
 		print_inventory_item(selected_item)
+	if Input.is_action_just_pressed("buy__inventory_item"):
+		if inventory[selected_item].quantity >= 1:
+			inventory[selected_item].quantity -= 1
+			print("Enjoy your " + inventory[selected_item].name + "!")
+		else:
+			print("I don't have any more of that item, buy something else.")
+		
+	if inventory[selected_item].quantity == 0:
+		delete_item(selected_item)
 
 
 func print_inventory_item(item_number : int):
@@ -30,11 +45,11 @@ func print_inventory_item(item_number : int):
 	print("I have " + str(inventory[item_number].quantity) + " of this item")
 	print("This item " + inventory[item_number].description)
 
-func add_item(inventory_item : Inventory_Item):
-	pass
+func add_item(new_item : Resource):
+	inventory.append(new_item)
 
-func delete_item():
-	pass
+func delete_item(done_item: int):
+	inventory.erase(done_item)
 
 # add to inventory
 # remove from inventory
--- scripts/door.gd
+++ scripts/door.gd
@@ -1,19 +1,19 @@
 extends AnimatableBody2D
-var open = false
-@onready var lever: Area2D = $"Lever"
-
-func _ready():
-	lever.door_open.connect(_on_door_open)
-
-func _on_door_open():
-	print("It Worked!")
-	set_is_open(true)
-func set_is_open(is_open: bool) -> void:
-	if is_open == true:
-		$AnimatedSprite2D.frame = 0
-		collision_layer = 1
-		$LightOccluder2D.visible = false
-	else:
-		$AnimatedSprite2D.frame = 1
-		collision_layer = 0
-		$LightOccluder2D.visible = true
+#var open = false
+#@onready var lever: Area2D = $"Lever"
+#
+#func _ready():
+	#lever.door_open.connect(_on_door_open)
+#
+#func _on_door_open():
+	#print("It Worked!")
+	#set_is_open(true)
+#func set_is_open(is_open: bool) -> void:
+	#if is_open == true:
+		#$AnimatedSprite2D.frame = 0
+		#collision_layer = 1
+		#$LightOccluder2D.visible = false
+	#else:
+		#$AnimatedSprite2D.frame = 1
+		#collision_layer = 0
+		#$LightOccluder2D.visible = true
